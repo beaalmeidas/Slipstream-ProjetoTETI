0052312cd0a625d773991f728363b4f7411f05d8
Beatriz Almeida <beatrizalmeidabss@gmail.com>
2025-10-07 02:29:39 -0300

    feat: implementing race routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import userRoutes from "./userRoutes";
 import teamRoutes from "./teamRoutes";
 import driverRoutes from "./driverRoutes";
+import raceRoutes from "./raceRoutes";
 
 
 const router = Router();
@@ -9,5 +10,6 @@ const router = Router();
 router.use("/user", userRoutes);
 router.use("/team", teamRoutes);
 router.use("/driver", driverRoutes);
+router.use("/race", raceRoutes);
 
 export default router;
